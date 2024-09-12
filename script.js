f6dae5ab70695fd974a55a854af69dce610a2dda
Andre-tz <italo.tenorio1999@gmail.com>
2024-09-12 14:20:15 -0500

    Se hizo la funcion para cambiar al modo noche, tambien se arreglaron errores como el contraste del special color y e le dio una animacion al momomento de que se cargue la pagina

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 
 const btnMenu=document.getElementById("btn-menu")
 const menu=document.getElementById("menu")
+const btnNight=document.querySelector("#btn-night-mode")
+const body=document.querySelector("#body")
 
 /*
 *EVENTOS
@@ -12,7 +14,9 @@ const menu=document.getElementById("menu")
 //Desplegar menu
 
 btnMenu.addEventListener("click", desplegarMenu)
+//Modo noche
 
+btnNight.addEventListener("click", nightMode)
 
 /*
 *FUNCIONES
@@ -21,4 +25,8 @@ btnMenu.addEventListener("click", desplegarMenu)
 //Desplegar menu
 function desplegarMenu(){
     menu.classList.toggle("active-list")
+}
+
+function nightMode(){
+    body.classList.toggle("night-mode")
 }
